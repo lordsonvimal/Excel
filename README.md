e98858d79cf701309ad7ead1c003d1d2d71ab101
lordsonvimal <lordsonvimal.yesudhas@sycamoreinformatics.com>
2021-01-31 23:54:34 +0530

    Added width of columns

--- README.md
+++ README.md
@@ -2,4 +2,30 @@
 
 Automate excel operations using python.
 
-- python version - 3.8
+- python version - 3.8.7
+
+## Setup
+
+##### 1. Pyenv
+ 
+`$ brew install openssl readline sqlite3 xz zlib`
+
+`$ curl https://pyenv.run | bash`
+
+`export PATH="$HOME/.pyenv/bin:$PATH"`
+
+`eval "$(pyenv init -)"`
+
+`eval "$(pyenv virtualenv-init -)"`
+
+`$ exec "$SHELL" # Or just restart your terminal`
+
+`$ pyenv install --list | grep " 3\.[678]" # Lists available python 3 versions`
+
+`$ pyenv install -v 3.8.7`
+
+##### 2. Clone this repository
+##### 3. Go inside project root directory. It should automatically switch to use `python 3.8.7`
+##### 4. `pip install pipenv`
+##### 5. Run `pipenv install`
+##### 6. Run `python excel.py`  
--- excel.py
+++ excel.py
@@ -1,25 +1,56 @@
 import pandas
 
-# pip install openpyxl
-def read(path):
-    data = pandas.read_excel(path, sheet_name=None)
-    print(data)
-    return data
+class Column:
+    def __init__(self, col_data, col_name, col_index):
+        self.data = col_data
+        self.index = col_index
+        self.name = col_name
 
-# pip install xlsxwriter
-def write(path, data_frame):
-    writer = pandas.ExcelWriter(path, engine="xlsxwriter")
-    data_frame.to_excel(writer, index=False)
-    # We can add any custom formatting here
-    # 1. Write formatting rules
-    # 2. Connect formatting functions here
-    writer.save()
+    def set_width(self, worksheet, col_width=-1, offset=0):
+        width = col_width if col_width > -1 else max(self.data.astype(str).map(len).max(), len(str(self.name))) + offset
+        worksheet.set_column(self.index, self.index, width)
 
-def dict_to_df(dict):
-    for key, value in dict.items():
-        yield pandas.DataFrame(value).T
+class ExcelSheet:
+    def __init__(self, data_frame, sheet_name, writer):
+        self.data_frame = data_frame
+        self.name = sheet_name
+        self.writer = writer
+
+    def iter_cols(self, col_name=""):
+        for index, col in enumerate(self.data_frame):
+            if (col_name == "") or (col_name == col):
+                column = Column(self.data_frame[col], col, index)
+                yield column
+
+    def to_excel(self):
+        self.data_frame.to_excel(self.writer, index=False, sheet_name=self.name)
+        worksheet = self.writer.sheets[self.name]
+        # We can add any custom formatting here
+        # 1. Write formatting rules
+        # 2. Connect formatting functions here
+        # 3. Move these formatting to separate functions
+        for column in self.iter_cols():
+            column.set_width(worksheet)
+
+class Excel:
+    def __init__(self, input_path, output_path):
+        self.data = pandas.read_excel(input_path, sheet_name=None)
+        self.writer = pandas.ExcelWriter(output_path, engine="xlsxwriter")
+        self.sheets = []
+
+    def update_sheets(self):
+        for key, value in self.data.items():
+            self.sheets.append(ExcelSheet(pandas.DataFrame(value), key, self.writer))
+
+    def save(self):
+        if len(self.sheets) > 0:
+            for sheet in self.sheets:
+                sheet.to_excel()
+            self.writer.save()
 
 # Sample to test the above fns
-data = read("test.xlsx")
-for item in dict_to_df(data):
-    write("new.xlsx", item)
+# This should go to a separate file
+# Adding it here for now
+excel = Excel("test.xlsx", "new.xlsx")
+excel.update_sheets()
+excel.save()
